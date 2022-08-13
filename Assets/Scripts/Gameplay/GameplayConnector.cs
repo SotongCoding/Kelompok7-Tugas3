5c9332bc77e5f57ca76be01d94bd3af68bc42ce3
Sugeng <game.sugeng@gmail.com>
2022-08-13 14:17:15 +0800

    Adding basic MVC

---
+++ Assets/Scripts/Gameplay/GameplayConnector.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Agate.MVC.Base;
+using Agate.MVC.Core;
+
+namespace ExampleGame.Gameplay
+{
+    public class GameplayConnector : BaseConnector
+    {
+        // private SaveDataController _saveData;
+        // private SoundfxController _soundfx;
+
+        // public void OnUpdateCoin(UpdateCoinMessage message)
+        // {
+        //     // _saveData.OnUpdateCoin(message.Coin);
+        //     // _soundfx.OnUpdateCoin();
+        // }
+
+        protected override void Connect()
+        {
+            //Subscribe<UpdateCoinMessage>(OnUpdateCoin);
+        }
+
+        protected override void Disconnect()
+        {
+            //Unsubscribe<UpdateCoinMessage>(OnUpdateCoin);
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/GameplayView.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Agate.MVC.Base;
+using Agate.MVC.Core;
+
+namespace ExampleGame.Gameplay
+{
+    public class GameplayView : BaseSceneView
+    {
+        //[SerializeField] public ClickGameView ClickGameView;
+    }
+}
